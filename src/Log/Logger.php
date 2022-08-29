5846c62f52896023f8c531d1bd11e4798ff42dc9
Ata <quetzalskeith@gmail.com>
2022-08-29 14:31:04 +0700

    Fix logger

--- src/Log/Logger.php
+++ src/Log/Logger.php
@@ -191,12 +191,12 @@ class Logger implements LoggerInterface
     /**
      * System is unusable.
      *
-     * @param  string  $message
-     * @param  array  $context
+     * @param string|\Stringable $message
+     * @param mixed[] $context
      *
      * @return void
      */
-    public function emergency($message, array $context = [])
+    public function emergency(string|\Stringable $message, array $context = []): void
     {
         $this->driver()->emergency($message, $context);
     }
@@ -207,12 +207,12 @@ class Logger implements LoggerInterface
      * Example: Entire website down, database unavailable, etc. This should
      * trigger the SMS alerts and wake you up.
      *
-     * @param  string  $message
-     * @param  array  $context
+     * @param string|\Stringable $message
+     * @param mixed[] $context
      *
      * @return void
      */
-    public function alert($message, array $context = [])
+    public function alert(string|\Stringable $message, array $context = []): void
     {
         $this->driver()->alert($message, $context);
     }
@@ -222,12 +222,12 @@ class Logger implements LoggerInterface
      *
      * Example: Application component unavailable, unexpected exception.
      *
-     * @param  string  $message
-     * @param  array  $context
+     * @param string|\Stringable $message
+     * @param mixed[] $context
      *
      * @return void
      */
-    public function critical($message, array $context = [])
+    public function critical(string|\Stringable $message, array $context = []): void
     {
         $this->driver()->critical($message, $context);
     }
@@ -236,12 +236,12 @@ class Logger implements LoggerInterface
      * Runtime errors that do not require immediate action but should typically
      * be logged and monitored.
      *
-     * @param  string  $message
-     * @param  array  $context
+     * @param string|\Stringable $message
+     * @param mixed[] $context
      *
      * @return void
      */
-    public function error($message, array $context = [])
+    public function error(string|\Stringable $message, array $context = []): void
     {
         $this->driver()->error($message, $context);
     }
@@ -252,25 +252,25 @@ class Logger implements LoggerInterface
      * Example: Use of deprecated APIs, poor use of an API, undesirable things
      * that are not necessarily wrong.
      *
-     * @param  string  $message
-     * @param  array  $context
+     * @param string|\Stringable $message
+     * @param mixed[] $context
      *
      * @return void
      */
-    public function warning($message, array $context = [])
+    public function warning(string|\Stringable $message, array $context = []): void
     {
         $this->driver()->warning($message, $context);
     }
 
-    /**
+     /**
      * Normal but significant events.
      *
-     * @param  string  $message
-     * @param  array  $context
+     * @param string|\Stringable $message
+     * @param mixed[] $context
      *
      * @return void
      */
-    public function notice($message, array $context = [])
+    public function notice(string|\Stringable $message, array $context = []): void
     {
         $this->driver()->notice($message, $context);
     }
@@ -280,12 +280,12 @@ class Logger implements LoggerInterface
      *
      * Example: User logs in, SQL logs.
      *
-     * @param  string  $message
-     * @param  array  $context
+     * @param string|\Stringable $message
+     * @param mixed[] $context
      *
      * @return void
      */
-    public function info($message, array $context = [])
+    public function info(string|\Stringable $message, array $context = []): void
     {
         $this->driver()->info($message, $context);
     }
@@ -293,12 +293,12 @@ class Logger implements LoggerInterface
     /**
      * Detailed debug information.
      *
-     * @param  string  $message
-     * @param  array  $context
+     * @param string|\Stringable $message
+     * @param mixed[] $context
      *
      * @return void
      */
-    public function debug($message, array $context = [])
+    public function debug(string|\Stringable $message, array $context = []): void
     {
         $this->driver()->debug($message, $context);
     }
@@ -306,13 +306,15 @@ class Logger implements LoggerInterface
     /**
      * Logs with an arbitrary level.
      *
-     * @param  mixed  $level
-     * @param  string  $message
-     * @param  array  $context
+     * @param mixed   $level
+     * @param string|\Stringable $message
+     * @param mixed[] $context
      *
      * @return void
+     *
+     * @throws \Psr\Log\InvalidArgumentException
      */
-    public function log($level, $message, array $context = [])
+    public function log($level, string|\Stringable $message, array $context = []): void
     {
         $this->driver()->log($level, $message, $context);
     }
